58526af963c96a7be4dcfe0446165ce7c7ddf6b9
ksaicharan <ksaicharan@gmail.com>
2018-02-02 22:36:57 -0800

    Initial Project Structure Push

---
+++ msg/src/main/java/tcloud/msg/App.java
@@ -0,0 +1,13 @@
+package tcloud.msg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
